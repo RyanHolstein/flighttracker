7b46638a24bf93b47534bc3c022e891c622e8cad
Ryan Holstein <ryan.holstein@gmail.com>
2014-03-25 15:59:12 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Skytime::Application.config.secret_key_base = '5eb42978a5659aad01af69f1898101da902de647998195c418cb808158652c7cbde82a7ec8d9f272e0efd43f1bf396e863f8da534742de3b0ff19f5ab5cffe6d'
